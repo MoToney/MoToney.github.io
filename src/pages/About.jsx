6ec98263942aaa5499d683296869883ef7a76ecf
mo <mauricetoneyjr@gmail.com>
2026-05-04 18:50:13 -0500

    first commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,27 @@ export default function About() {
         <section>
             <h1> About Me </h1>
                 <p>
-                    Hello I'm a recent Computer Science graduate
+                    Hello, my name is Maurice Toney I am a recent graduate with a Bachelor’s of Science in Computer Science.
+                    I am currently based in the Houston,Texas Metro area, and I’m from the Austin, Texas Metro Area
+                    with my roots being in New Orleans, Louisiana.
+                </p>
+
+                <p>
+                    I am well-versed in many areas related to computers but I lean heaviest towards software.
+                    In particular I am skilled in Python, Java, and C++, with
+                    experience building full-stack applications and machine learning models.
+                </p>
+
+                <p>
+                    Outside of programming and computer science as a whole,  I like to think I’m a little enigmatic.
+                    I love so-called “nerdy” things like History, Harry Potter, Star Wars, Game of Thrones, Lord of the Rings,
+                    Percy Jackson, comics, Dragon Ball Z, Marvel easter egg videos, etc … but I also love the UFC,
+                    basketball, boxing, and the New Orleans Saints.
+                </p>
+
+                <p>
+                    I believe for someone I’m yet to meet, the most self-illuminating characteristic I can share about
+                    myself is my desire to be a positive force in the world and more specifically my community.
                 </p>
 
         </section>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,14 +1,19 @@
 import {projects} from "../data/projects.js";
 import ProjectCard from "../components/ProjectCard.jsx";
+import "../styles/Projects.css";
 
 
 export default function Projects() {
     return (
         <section>
             <h1>Projects</h1>
-            {projects.map((project, index) => (
-                <ProjectCard key={index} {...project} />
-            ))}
+
+            <div className="projects-grid">
+                {projects.map((project, index) => (
+                    <ProjectCard key={index} {...project} />
+                ))}
+            </div>
+
         </section>
     )
 }
