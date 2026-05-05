81c9111e887cc0d0b4d89f047c9b85625af990c2
mo <mauricetoneyjr@gmail.com>
2026-05-04 21:10:27 -0500

    correcting vercel

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,3 @@
 export default function handler(req, res) {
-    res.status(200).json({ message: "Hello from Vercel backend" });
+    res.status(200).json({ message: "Hello from Verel backend" });
 }
