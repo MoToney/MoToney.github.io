86c33444f6faf02f20c1aaee0c1295962a576923
mo <mauricetoneyjr@gmail.com>
2026-05-05 22:04:28 -0500

    toggl integration

--- api/weekly.js
+++ api/weekly.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
         }
 
         res.status(200).json(data);
-    } catch (err) {
+    } catch ( err ) {
         res.status(500).send("Error fetching data");
     }
 }
